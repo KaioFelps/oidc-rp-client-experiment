f52fa41aa5b5a562cf076c5172a984c91c1c2de7
Kaio Felps <kaiofelipe2005@gmail.com>
2025-11-04 17:11:40 -0300

    fix: add /oidc path to issuer

--- src/configs/client.ts
+++ src/configs/client.ts
@@ -6,7 +6,7 @@
  * [lti-consumer-poc]: https://github.com/KaioFelps/lti-consumer-poc
  */
 export const OidcClientConfig = Object.freeze({
-    issuer: new URL("http://localhost:3000"),
+    issuer: new URL("http://localhost:3000/oidc"),
     algorithm: "oidc",
     clientId: "test-client-1",
     clientSecret: "test-client-1-secret",
